a0a7de6c26cbdc4fa84a0ac0acf24b8ad539ad20
Bo Chen <412862016@qq.com>
2018-08-20 13:33:53 +0800

    split convertACubicBezierCurveToArcsHelper function

--- src/bezierCurveToArcs/BezierCurveToArcs.java
+++ src/bezierCurveToArcs/BezierCurveToArcs.java
@@ -22,6 +22,8 @@ import java.util.ArrayList;
  */
 public class BezierCurveToArcs {
 
+    private static final double AllOWABLE_ERROR_FOR_FIND_T = 0.001;
+
     /**
      * To convert a cubic Bezier curve to a series of arcs.
      *
@@ -75,117 +77,46 @@ public class BezierCurveToArcs {
 
         /* Step 1: Calculate the new start point and the new end point of the
         current circumstance */
-        DoublePoint newA
+        DoublePoint A0
                 = CubicBezierTools.pointOnBezierCurve(startT,
                 A, controlPointA, controlPointB, B);
 
-        DoublePoint newB = CubicBezierTools.pointOnBezierCurve(endT,
+        DoublePoint A1 = CubicBezierTools.pointOnBezierCurve(endT,
                 A, controlPointA, controlPointB, B);
 
-        /* Step 2: Calculate the unit tangent vector of the Bezier curve on the
-        start point and the end point */
-        DoublePoint unitTangentVector0
-                = CubicBezierTools.calculateUnitTangentVectorOfBezierCurve(
-                A, controlPointA, controlPointB, B, startT);
-
-        DoublePoint unitTangentVector1
-                = CubicBezierTools.calculateUnitTangentVectorOfBezierCurve(
-                A, controlPointA, controlPointB, B, endT);
-
-        DoublePoint adjacentNewA
-                = new DoublePoint(newA.getX() + unitTangentVector0.getX(),
-                newA.getY() + unitTangentVector0.getY());
-
-        DoublePoint adjacentNewB
-                = new DoublePoint(newB.getX() + unitTangentVector1.getX(),
-                newB.getY() + unitTangentVector1.getY());
-
-        /* Step 3: Calculate the intersection of the two tangent line from the
-        start point and the end point*/
-        DoublePoint V = MathTools.calculateIntersectionOfTwoLine(
-                newA, adjacentNewA, adjacentNewB, newB);
-
-        /* Step 4: Find incenter of the triangle A0A1V */
-        DoublePoint G = MathTools.findInCenterPointOfTriangle(newA, V, newB);
-
-        /* Step 5: Find two centers of biarc */
-        double a11 = V.getX() - newA.getX();
-        double a12 = V.getY() - newA.getY();
-        double b1 = newA.getX() * a11 + newA.getY() * a12;
-        double a21 = G.getX() - newA.getX();
-        double a22 = G.getY() - newA.getY();
-        double b2 = (G.getX() + newA.getX()) / 2.0 * a21
-                + (G.getY() + newA.getY()) / 2.0 * a22;
-        DoublePoint center1
-                = MathTools.solveLinearEquationsOfTwoUnknownVariables(
-                a11, a12, b1, a21, a22, b2);
-
-        a11 = V.getX() - newB.getX();
-        a12 = V.getY() - newB.getY();
-        b1 = newB.getX() * a11 + newB.getY() * a12;
-        a21 = G.getX() - newB.getX();
-        a22 = G.getY() - newB.getY();
-        b2 = (G.getX() + newB.getX()) / 2.0 * a21
-                + (G.getY() + newB.getY()) / 2.0 * a22;
-        DoublePoint center2
-                = MathTools.solveLinearEquationsOfTwoUnknownVariables(
-                a11, a12, b1, a21, a22, b2);
-
-        /* Step 6: Calculate the unit tangent vector of the circle on point G */
-        DoublePoint H = MathTools.calculateUnitTangentVectorOfCircle(center1, G);
-
-        /* Step 7: Calculate t */
-        double t = findTWithNewtonAndRaphsonMethod(
-                A, controlPointA, controlPointB, B, H, G, 0.001,
+        /* Step 2: Calculate the intersection of the two tangent lines from the
+        new start point and the end point*/
+        DoublePoint V = calculateV(A0, A1, A, controlPointA, controlPointB, B,
                 startT, endT);
 
+        /* Step 3: Find the incenter of the triangle A0A1V */
+        DoublePoint G = MathTools.findInCenterPointOfTriangle(A0, V, A1);
 
-        /* Step 8: Calculate d */
-        DoublePoint Q_t = CubicBezierTools.pointOnBezierCurve(t,
-                A, controlPointA, controlPointB, B);
-        double d = MathTools.euclideanDistance(Q_t, G);
+        /* Step 4: Find two centers of biarc */
+        DoublePoint center1 = calculateCenterOfArc(A0, V, G);
+        DoublePoint center2 = calculateCenterOfArc(A1, V, G);
 
-        /* Step 9: Judge if the current approximation meets the allowable error */
-        if (d <= allowableError) {
+        /* Step 5: Calculate the unit tangent vector of the circle on point G */
+        DoublePoint H = MathTools.calculateUnitTangentVectorOfCircle(center1, G);
 
-            double radius1 = MathTools.euclideanDistance(center1, G);
-            double radius2 = MathTools.euclideanDistance(center2, G);
+        /* Step 6: Calculate t */
+        double t = findTWithNewtonAndRaphsonMethod(
+                A, controlPointA, controlPointB, B, H, G, AllOWABLE_ERROR_FOR_FIND_T,
+                startT, endT);
 
-            double startAngle1
-                    = MathTools.calculateAngleRelativeToCircleCenter(center1, newA);
-            double endAngle1
-                    = MathTools.calculateAngleRelativeToCircleCenter(center1, newB);
+        /* Step 7: Calculate max error between the fitted arcs and the original
+        Bezier curve */
+        double maxError = calculateMaxError(
+                A, controlPointA, controlPointB, B, G, t);
 
-            while (endAngle1 < startAngle1 - Math.PI) {
-                endAngle1 += 2.0 * Math.PI;
-            }
-            while (endAngle1 > startAngle1 + Math.PI) {
-                endAngle1 -= 2.0 * Math.PI;
-            }
+        /* Step 8: Judge if the current approximation meets the allowable error */
+        if (maxError <= allowableError) {
 
-            if (startAngle1 <= endAngle1) {
-                arcs.add(new Arc(center1, radius1, startAngle1, endAngle1, false));
-            } else {
-                arcs.add(new Arc(center1, radius1, startAngle1, endAngle1, true));
-            }
+            Arc firstArc = generateArc(center1, A0, G);
+            Arc secondArc = generateArc(center2, G, A1);
 
-            double startAngle2
-                    = MathTools.calculateAngleRelativeToCircleCenter(center2, newA);
-            double endAngle2
-                    = MathTools.calculateAngleRelativeToCircleCenter(center2, newB);
-
-            while (endAngle2 < startAngle2 - Math.PI) {
-                endAngle2 += 2.0 * Math.PI;
-            }
-            while (endAngle2 > startAngle2 + Math.PI) {
-                endAngle2 -= 2.0 * Math.PI;
-            }
-
-            if (startAngle2 <= endAngle2) {
-                arcs.add(new Arc(center2, radius2, startAngle2, endAngle2, false));
-            } else {
-                arcs.add(new Arc(center2, radius2, startAngle2, endAngle2, true));
-            }
+            arcs.add(firstArc);
+            arcs.add(secondArc);
 
         } else {
 
@@ -198,6 +129,32 @@ public class BezierCurveToArcs {
         }
     }
 
+    /**
+     * To find the t value so that (Q(t) - G) · H = 0, where
+     * Q(t) = P0 * (1-t)^3 + P1 * 3t(1-t)^2 + P2 * 3t^2(1-t) + P3 * t^3. (P0,
+     * P1, P2, P3 are the start point, the first control point, the second
+     * control point and the end point of the current Bezier curve).
+     * <p>
+     * Newton-Raphson method is used in this function because
+     * f(t) = (Q(t) - G) · H is monotone for t which lies in the range
+     * [{@code startT}, {@code endT}. In Newton-Raphson method,
+     * tn+1 = tn - f(tn)/f'(tn).
+     *
+     * @param A              the start point of the Bezier curve
+     * @param controlPointA  the first control point which is close to the
+     *                       start point
+     * @param controlPointB  the second control point which is close to the
+     *                       end point
+     * @param B              the end point of the Bezier curve
+     * @param H              the unit tangent vector on point {@code G} of the
+     *                       biarc
+     * @param G              the intersection point of biarc
+     * @param allowableError the allowable error for
+     *                       f(t) <= {@code allowableError}
+     * @param startT         the t parameter which determines the start position
+     * @param endT           the t parameter which determines the end position
+     * @return the t value which makes f(t) is near to zero
+     */
     private static double findTWithNewtonAndRaphsonMethod(
             DoublePoint A, DoublePoint controlPointA,
             DoublePoint controlPointB, DoublePoint B,
@@ -230,5 +187,128 @@ public class BezierCurveToArcs {
         }
 
         return tn;
+    }
+
+    /**
+     * Let the point {@code newA} be the start point of the current Bezier
+     * curve and the point {@code newB} be the end point of the current Bezier
+     * curve. Let L1 be the tangent line to the Bezier curve on the point
+     * {@code newA} and L2 be the tangent line to the Bezier curve on the point
+     * {@code newB}. Then the point V is the intersection point of L1, L2. This
+     * function is to calculate the coordinate of V.
+     *
+     * @param newA          the new start point of the Bezier curve
+     * @param newB          the new end point of the Bezier curve
+     * @param A             the original start point of the Bezier curve
+     * @param controlPointA the first control point of the Bezier curve which
+     *                      is close to {@code A}
+     * @param controlPointB the second control point of the Bezier curve which
+     *                      is close to {@code B}
+     * @param B             the original end point of the Bezier curve
+     * @param startT        the t parameter which determines the position of
+     *                      the point {@code newA}
+     * @param endT          the t parameter which determines the position of
+     *                      the point {@code newB}
+     * @return the coordinate of V
+     */
+    private static DoublePoint calculateV(
+            DoublePoint newA, DoublePoint newB,
+            DoublePoint A, DoublePoint controlPointA,
+            DoublePoint controlPointB, DoublePoint B,
+            double startT, double endT) {
+
+        DoublePoint unitTangentVector0
+                = CubicBezierTools.calculateUnitTangentVectorOfBezierCurve(
+                A, controlPointA, controlPointB, B, startT);
+
+        DoublePoint unitTangentVector1
+                = CubicBezierTools.calculateUnitTangentVectorOfBezierCurve(
+                A, controlPointA, controlPointB, B, endT);
+
+        DoublePoint adjacentNewA
+                = new DoublePoint(newA.getX() + unitTangentVector0.getX(),
+                newA.getY() + unitTangentVector0.getY());
+
+        DoublePoint adjacentNewB
+                = new DoublePoint(newB.getX() + unitTangentVector1.getX(),
+                newB.getY() + unitTangentVector1.getY());
+
+        return MathTools.calculateIntersectionOfTwoLine(
+                newA, adjacentNewA, adjacentNewB, newB);
+    }
+
+    /**
+     * Find the center O of a circle which makes the point {@code A} and the point
+     * {@code G} on the circle and the line OA is vertical to the line AV.
+     *
+     * @param A the first point
+     * @param V the second point
+     * @param G the third point
+     * @return the center of the circle
+     */
+    private static DoublePoint calculateCenterOfArc(
+            DoublePoint A, DoublePoint V, DoublePoint G) {
+
+        double a11 = V.getX() - A.getX();
+        double a12 = V.getY() - A.getY();
+        double b1 = A.getX() * a11 + A.getY() * a12;
+
+        double a21 = G.getX() - A.getX();
+        double a22 = G.getY() - A.getY();
+        double b2 = (G.getX() + A.getX()) / 2.0 * a21
+                + (G.getY() + A.getY()) / 2.0 * a22;
+
+        return MathTools.solveLinearEquationsOfTwoUnknownVariables(
+                a11, a12, b1, a21, a22, b2);
+    }
+
+    /**
+     * This method is to calculate the max error between the original Bezier
+     * curve and the fitted arcs.
+     *
+     * @param A             the start point of the Bezier curve
+     * @param controlPointA the first control point of the Bezier curve which
+     *                      is close to {@code A}
+     * @param controlPointB the second control point of the Bezier curve which
+     *                      is close to {@code B}
+     * @param B             the end point of the Bezier curve
+     * @param G             the incenter point
+     * @param t             the t value which makes f(t) = (Q(t) - G) · H = 0
+     * @return the max error between the original Bezier curve and the fitted
+     * arcs.
+     */
+    private static double calculateMaxError(
+            DoublePoint A, DoublePoint controlPointA,
+            DoublePoint controlPointB, DoublePoint B,
+            DoublePoint G, double t) {
+
+        DoublePoint Q_t = CubicBezierTools.pointOnBezierCurve(t,
+                A, controlPointA, controlPointB, B);
+
+        return MathTools.euclideanDistance(Q_t, G);
+    }
+
+    private static Arc generateArc(
+            DoublePoint center, DoublePoint startPoint, DoublePoint endPoint) {
+
+        double radius = MathTools.euclideanDistance(center, endPoint);
+
+        double startAngle
+                = MathTools.calculateAngleRelativeToCircleCenter(center, startPoint);
+        double endAngle
+                = MathTools.calculateAngleRelativeToCircleCenter(center, endPoint);
+
+        while (endAngle < startAngle - Math.PI) {
+            endAngle += 2.0 * Math.PI;
+        }
+        while (endAngle > startAngle + Math.PI) {
+            endAngle -= 2.0 * Math.PI;
+        }
+
+        if (startAngle <= endAngle) {
+            return new Arc(center, radius, startAngle, endAngle, false);
+        } else {
+            return new Arc(center, radius, startAngle, endAngle, true);
+        }
     }
 }
